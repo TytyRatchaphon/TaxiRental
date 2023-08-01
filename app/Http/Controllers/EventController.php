79b9f44c3e1e717dc06f3febbcd91fb9b757d1dc
eviperz <katin2200@hotmail.com>
2023-08-01 20:42:42 +0700

    create EventController

---
+++ app/Http/Controllers/EventController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    public function index() {
+        return view('events.index');
+    }
+    public function show() {
+        return view('events.show');
+    }
+    public function manageKanban() {
+        return view('events.manage.kanban');
+    }
+    public function manageApplicants() {
+        return view('events.manage.manage-applicants');
+    }
+    public function manageStaffs() {
+        return view('events.manage.manage-staffs');
+    }
+    public function manageBudgets() {
+        return view('events.manage.manage-budgets');
+    }
+}
