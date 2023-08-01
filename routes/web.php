b9d0969a22a91f1ed28c5bade627e88f80ac2d60
eviperz <katin2200@hotmail.com>
2023-08-01 20:43:05 +0700

    add route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EventController;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Support\Facades\Route;
 use App\Models\Event;
@@ -27,6 +28,13 @@ Route::get('/search-events', [HomeController::class, 'searchEvents']);
 Route::get('/login', function () {
     return view('login');
 })->middleware(['auth','verified'])->name('login');
+
+Route::get('/events', [EventController::class, 'index'])->name('events.index');
+Route::get('/events/show', [EventController::class, 'show'])->name('events.show');
+Route::get('/events/event/manage/kanban', [EventController::class, 'manageKanban'])->name('events.manage.kanban');
+Route::get('/events/event/manage/applicants', [EventController::class, 'manageApplicants'])->name('events.manage.applicants');
+Route::get('/events/event/manage/staffs', [EventController::class, 'manageStaffs'])->name('events.manage.staffs');
+Route::get('/events/event/manage/budgets', [EventController::class, 'manageBudgets'])->name('events.manage.budgets');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
