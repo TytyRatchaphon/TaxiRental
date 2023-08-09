56a1cdde81e6a1ae3ef83e9c4f9c356e155bc070
eviperz <jirapa.wong@ku.th>
2023-08-10 04:09:21 +0700

    update factory and seeder for kanbans

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(EventSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(KanbanSeeder::class);
     }
 }
--- database/seeders/KanbanSeeder.php
+++ database/seeders/KanbanSeeder.php
@@ -4,16 +4,15 @@ namespace Database\Seeders;
 
 use App\Models\Event;
 use App\Models\Kanban;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class EventSeeder extends Seeder
+class KanbanSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $events = Event::factory()->count(10)->has(Kanban::factory(10))->create();
+        $kanbans = Kanban::factory()->count(10)->create();
     }
 }
