3ed9cbd90d516c85e861b84225fc8088d7414642
eviperz <jirapa.wong@ku.th>
2023-08-18 23:00:17 +0700

    update show and edit profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -1,8 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -14,6 +13,10 @@ class ProfileController extends Controller
     /**
      * Display the user's profile form.
      */
+    public function index() {
+        $user = Auth::user();
+        return view('profile.index', ['user' => $user]);
+    }
     public function edit(Request $request): View
     {
         return view('profile.edit', [
@@ -24,17 +27,50 @@ class ProfileController extends Controller
     /**
      * Update the user's profile information.
      */
-    public function update(ProfileUpdateRequest $request): RedirectResponse
+    public function update(Request $request, User $user)
     {
-        $request->user()->fill($request->validated());
+        $request->validate([
+            'username' => ['required', 'string', 'min:3', 'max:255'],
+            'user_profile_img' => ['nullable', 'image', 'mimes:jpeg', 'png', 'jpg', 'gif', 'max:2048'],
+            'user_firstname' => ['required', 'string', 'min:3', 'max:255'],
+            'user_lastname' => ['required', 'string',  'min:3', 'max:255'],
+            'major' => ['required', 'string', 'min:3', 'max:255'],
+            'faculty' => ['required', 'string', 'min:3', 'max:255'],
+            'year' => ['required', 'integer', 'min:1', 'max:4'],
+            'facebook' => ['nullable', 'string', 'min:3', 'max:255'],
+            'line' => ['nullable', 'string', 'min:3', 'max:255'],
+            'instagram' => ['nullable', 'string', 'min:3', 'max:255'],
+            
+        ]);
+
+        $user->username = $request->get('username');
+        $user->user_firstname = $request->get('user_firstname');
+        $user->user_lastname = $request->get('user_lastname');
+        $user->user_profile_img = $request->get('user_profile_img');
 
         if ($request->user()->isDirty('email')) {
             $request->user()->email_verified_at = null;
+        } else {
+            $user->email = $request->get('email');
+
         }
 
-        $request->user()->save();
+        if ($request->get('password') !== null) {
+            $user->password = $request->get('password');
+        }
 
-        return Redirect::route('profile.edit')->with('status', 'profile-updated');
+        if ($user->role === "USER") {
+            $student = $user->student;
+            $student->major = $request->get('major');
+            $student->faculty = $request->get('faculty');
+            $student->year = $request->get('year');
+            $student->facebook = $request->get('facebook');
+            $student->line = $request->get('line');
+            $student->instagram = $request->get('instagram');
+        }
+
+        $user->save();
+        return redirect()->route('profile.index');
     }
 
     /**
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,143 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
+@extends('layouts.main')
+@section('content')
+<div class="p-10">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <form method="POST" action="{{ route('profile.update', ['user' => $user]) }}">
+            @csrf
+            @method('put')
+            <!-- Profile Image -->
+            <div class="max-w-sm mx-auto overflow-hidden">
+                <img id="profileImagePreview"
+                    src="{{ asset('storage/' . (old('user_profile_img', $user->user_profile_img) ?: $user->user_profile_img)) }}"
+                    alt="user_profile_img" class="h-48 w-48 rounded-full border-4 border-yellow-300 mx-auto my-5">
+                <x-input-label for="user_profile" :value="__('Profile Image')" class="text-center" />
+                    <input id="user_profile_img" class="block py-2 px-4 shadow-none" type="file"
+                        name="user_profile_img" onchange="previewProfileImage(event)" />
+                @error('user_profile_img')
+                <p class="mt-2 text-red-500 text-sm">{{ $message }}</p>
+                @enderror
+            </div>
+            <!-- First Name -->
+            <div class="mb-6">
+                <x-input-label for="user_firstname" :value="__('First Name')" />
+                <x-text-input id="user_firstname" class="block w-full mt-1" type="text" name="user_firstname"
+                    :value="old('user_firstname', $user->user_firstname)" autofocus />
+                @error('user_firstname')
+                <p class="mt-2 text-red-500 text-sm">{{ $message }}</p>
+                @enderror
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
+            <!-- Last Name -->
+            <div class="mb-6">
+                <x-input-label for="user_lastname" :value="__('Last Name')" />
+                <x-text-input id="user_lastname" class="block w-full mt-1" type="text" name="user_lastname"
+                    :value="old('user_lastname', $user->user_lastname)"/>
+                @error('user_lastname')
+                <p class="mt-2 text-red-500 text-sm">{{ $message }}</p>
+                @enderror
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
+            <!-- Username -->
+            <div class="mb-6">
+                <x-input-label for="username" :value="__('Username')" />
+                <x-text-input id="username" class="block w-full mt-1" type="text" name="username"
+                    :value="old('username', $user->username)"/>
+                @error('username')
+                <p class="mt-2 text-red-500 text-sm">{{ $message }}</p>
+                @enderror
             </div>
-        </div>
+
+            @if($user->role === "USER")
+            <!-- Major -->
+            <div class="mb-6">
+                <x-input-label for="major" :value="__('Major')" />
+                <x-text-input id="major" class="block w-full mt-1" type="text" name="major"
+                    :value="old('major', $user->student->major)"/>
+                @error('major')
+                <p class="mt-2 text-red-500 text-sm">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <!-- Faculty -->
+            <div class="mb-6">
+                <x-input-label for="faculty" :value="__('Faculty')" />
+                <x-text-input id="faculty" class="block w-full mt-1" type="text" name="faculty"
+                    :value="old('faculty', $user->student->faculty)"/>
+                @error('faculty')
+                <p class="mt-2 text-red-500 text-sm">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <!-- Year -->
+            <div class="mb-6">
+                <x-input-label for="year" :value="__('Year')" />
+                <x-text-input id="year" class="block w-full mt-1" type="number" name="year"
+                    :value="old('year', $user->student->year)" min="1" max="4"/>
+                @error('year')
+                <p class="mt-2 text-red-500 text-sm">{{ $message }}</p>
+                @enderror
+            </div>
+            
+            @endif
+
+            <!-- Email Address -->
+            <div class="mt-4">
+                <x-input-label for="email" :value="__('Email')" />
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email"
+                    :value="old('email', $user->email)" autocomplete="username" />
+                @error('email')
+                <p class="mt-2 text-red-500 text-sm">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <!-- Password -->
+            <div class="mt-4">
+                <x-input-label for="password" :value="__('Password')" />
+                <x-text-input id="password" class="block mt-1 w-full" type="password" name="password"
+                    autocomplete="new-password" />
+
+                @error('password')
+                <p class="mt-2 text-red-500 text-sm">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <!-- Confirm Password -->
+            <div class="mt-4">
+                <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                    name="password_confirmation" autocomplete="new-password" />
+
+                @error('password_confirmation')
+                <p class="mt-2 text-red-500 text-sm">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <a class="underline text-sm text-black hover:text-[#F6D106]  rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
+                    href="{{ route('login') }}">
+                    Back to Profile
+                </a>
+                <button type="submit" class="ml-4 bg-[#F6D106] hover:bg-yellow-500 hover:scale-105">
+                    Save
+                <button>
+            </div>
+        </form>
     </div>
-</x-app-layout>
+    
+</div>
+
+@endsection
+<script>
+function previewProfileImage(event) {
+    const previewImage = document.getElementById('profileImagePreview');
+    const selectedFile = event.target.files[0];
+
+    if (selectedFile) {
+        const objectURL = URL.createObjectURL(selectedFile);
+        previewImage.src = objectURL;
+    } else {
+        previewImage.src = "{{ asset('storage/' . $user->user_profile_img) }}";
+    }
+}
+</script>
